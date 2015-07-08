128db7946b44a2f4e9b8bc99ba3bec3f4d9b67e1
Andreas Zweili <andreas@2li.ch>
2015-07-09 00:19:44 +0200

    fix a sync bug

---
+++ pet_variables.py
@@ -0,0 +1,23 @@
+# The pet's stats
+pet_name = "Fluffy"
+pet_photo = "<`)))><"
+pet_status = "youngling"
+pet_health = 5
+pet_age = 0
+pet_hunger = 5
+pet_happiness = 5
+pet_stomach
+
+# age based max values
+max_health = 5
+max_hunger = 5
+max_happiness = 10
+
+# Pictures and symbols used ingame
+cat = "(=^o.o^=)__"
+mouse = "<:3 )~~~~"
+fish = "<`)))><"
+owl = "(^0M0^)"
+
+# programme variables
+beginning_finished = False
